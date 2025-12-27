13dbd274f9d2d20fbfc260d125ac0fdee64813c5
2cdn1204-afk <2cdn1204@gmail.com>
2025-12-27 22:47:54 +0900

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,24 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const filename = fileURLToPath(import.meta.url);
+const getDirname = dirname(filename);
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(dirname, 'index.html'),
-        start_j: resolve(dirname, 'strart_j.html'),
-        hokkaido: resolve(dirname, 'Hokkaido.html'),
-        kanto: resolve(dirname, 'Kanto.html'),
-        kinki: resolve(dirname, 'kinki.html'),
-        map: resolve(dirname, 'map.html'),
-        okinawa: resolve(dirname, 'okinawa.html'),
-        sikoku: resolve(dirname, 'sikoku.html'),
-        tyuubu: resolve(__dirname, 'tyuubu.html'),
+        main: resolve(getDirname, 'index.html'),
+        start_j: resolve(getDirname, 'strart_j.html'),
+        hokkaido: resolve(getDirname, 'Hokkaido.html'),
+        kanto: resolve(getDirname, 'Kanto.html'),
+        kinki: resolve(getDirname, 'kinki.html'),
+        map: resolve(getDirname, 'map.html'),
+        okinawa: resolve(getDirname, 'okinawa.html'),
+        sikoku: resolve(getDirname, 'sikoku.html'),
+        tyuubu: resolve(getDirname, 'tyuubu.html'),
       },
     },
   },
