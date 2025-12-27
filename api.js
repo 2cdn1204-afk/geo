5c27fc81c4fa2fcb9d2846c78a9e1b98361693ca
2cdn1204-afk <2cdn1204@gmail.com>
2025-12-27 22:01:35 +0900

    Add files via upload

--- api.js
+++ api.js
@@ -1,12 +1,14 @@
+const google_key = import.meta.env.VITE_GOOGLE_KEY;
+
+
 function loadScript(apikey) {
- let script = document.createElement('script');
- script.src = "https://maps.googleapis.com/maps/api/js?key=" + apikey + "&callback=initialize&libraries=&v=weekly";
- script.async = true;
- document.body.append(script);
+  let script = document.createElement('script');
+  script.src = "https://maps.googleapis.com/maps/api/js?key=" + apikey + "&callback=initialize&libraries=&v=weekly";
+  script.async = true;
+  document.body.append(script);
 }
 
-let API_KEY = ""; // APIキーをここに入力
-loadScript(API_KEY);
+loadScript(google_key);
 
 // ゲームスタート時に4択をセット
 setChoices();
